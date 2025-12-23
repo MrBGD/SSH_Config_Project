1e890bb1602e87936bca7d26a814ca6817946945
MrBGD <bogdannae2006@gmail.com>
2025-12-23 18:30:30 -0500

    fisier config si un script ce afiseaza numele hostului si verifica daca hostname ul este o adresa ip sau dns valida

--- script.sh
+++ script.sh
@@ -1,12 +1,29 @@
+#!/bin/bash
+
+
 file=$1
 
 while read -r line; do
 
     case $line in
         *"Host "* )
-            y=$(echo $line | awk '{print $2}')
-            echo "The name of the host: $y"
+            host=$(echo $line | awk '{print $2}')
+            echo -e '\nThe name of the host: $host'
          ;;
+
+         *"HostName "* )
+
+            ip=$(echo $line | awk '{print $2}')
+            if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+                echo "Ip address of the host: $ip"
+            elif [[ $ip =~ ^([a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$ ]]; then
+                echo "DNS address of the host: $ip"
+            else
+                echo "Invalid Ip address"
+            fi
+         ;;
+
+
     esac 
 
 
